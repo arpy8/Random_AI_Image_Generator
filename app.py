891cdaca9267763f90c042d99fe1d776b70abd65
Arpit <arpitsengar99@gmail.com>
2023-07-13 20:11:35 +0530

    updated main.py

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
-from time import sleep
+import time
 import streamlit as st
-from stqdm import stqdm
 from brain import generate_image, temp_prompt, prompt_generated, image_generated
 
 ######################### STREAMLIT #########################
@@ -16,23 +15,35 @@ hide_streamlit_style = """
 st.markdown(hide_streamlit_style, unsafe_allow_html=True)
 
 prompt_loading_text = st.empty()
-prompt_loading_text.caption("# Cooking a prompt...")
-for _ in stqdm(range(50)):
-    sleep(0.3)
+prompt_loading_text.caption("# Cooking an amazing prompt...🤩")
+
+prg = st.empty()
+
+prg1 = prg.progress(0)
+for i in range(100):
+    time.sleep(0.12)
+    prg1.progress(i + 1)
     if prompt_generated:
         break
 
-prompt_loading_text.caption("# Generating a beautiful image")
-for _ in stqdm(range(50)):
-    sleep(0.3)
+prompt_loading_text.caption("# Generating a beautiful image...✨")
+
+prg1 = prg.progress(0)
+for i in range(100):
+    time.sleep(0.15)
+    prg1.progress(i + 1)
     if image_generated:
         break
 
-prompt_loading_text.caption("# Finalizing everything...")
-for _ in stqdm(range(50)):
-    sleep(0.3)
+prompt_loading_text.caption("# Finalizing everything...⚙️")
 
-prompt_loading_text.caption("#  Here's a random ai generated image")
+prg1 = prg.progress(0)
+for i in range(100):
+    time.sleep(0.1)
+    prg1.progress(i + 1)
+prg.empty()
+
+prompt_loading_text.caption("#  Here's your amazing ai generated image 💘")
 
 generate_image()
 st.image("temp.png")
